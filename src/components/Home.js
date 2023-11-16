261d6d4e0b8c17f47ea968e98695e3bdee126084
Shayan <biffgarfield21@gmail.com>
2023-11-16 19:35:08 +0530

    Overlay added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-
 export default function Home() {
   return (
     <>
-    <div class="bg-overlay"></div>
-    <div className="home-container">
-      <div className="line1">Search Between More Than</div>
-      <div className="line2">10,000+ Open Jobs.</div>
-      <div className="rectangular-box"></div>
-      
-    </div>
+      <div className="overlay">
+        <div className="home-text">
+          <div className="line1">Search Between More Than</div>
+          <div className="line2">10,000+ Open Jobs.</div>
+          
+        </div>
+      </div>
     </>
   );
 }
